bbed1f48ad15bf2df991f5e0118032a6f590fec2
s.kryvych <s.kryvych@plarium.com>
2020-10-24 16:18:36 +0300

    Running cats

--- Unity/Assets/Scripts/CatControl.cs
+++ Unity/Assets/Scripts/CatControl.cs
@@ -1,32 +1,77 @@
 ﻿using System;
 using System.Collections.Generic;
+using DefaultNamespace.CatStates;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 [RequireComponent(typeof(Collider), typeof(Rigidbody))]
 public class CatControl : MonoBehaviour
 {
   public static readonly HashSet<CatControl> AliveCats = new HashSet<CatControl>();
-  private Collider _collider;
-  private Rigidbody _body;
+  [NonSerialized]
+  public Collider Collider;
+  [NonSerialized]
+  public Rigidbody Body;
+
+
+  
+  private List<CatStateBase> _allStates;
+  private CatStateBase _currentState;
+  private float _changeStateDur;
+
 
   private void Awake()
   {
-    _collider = GetComponent<Collider>();
-    _body = GetComponent<Rigidbody>();
+    Collider = GetComponent<Collider>();
+    Body = GetComponent<Rigidbody>();
+    _allStates = new List<CatStateBase>()
+    {
+      new CatStateIdle(this),
+      new CatStateJump(this),
+      new CatStateRunAround(this),
+      new CatStateRunAwayFromPlayer(this),
+    };
+
+    SelectRandomState();
   }
 
   public void PrepareToBePart()
   {
-    _collider.enabled = false;
-    _body.isKinematic = true;
+    Collider.enabled = false;
+    Body.isKinematic = true;
     AliveCats.Remove(this);
+    _currentState = null;
+    _changeStateDur = float.MaxValue;
   }
 
   public void PrepareToBeReal()
   {
     AliveCats.Add(this);
-    _collider.enabled = true;
-    _body.isKinematic = false;
+    Collider.enabled = true;
+    Body.isKinematic = false;
+    
+    SelectRandomState();
+  }
+
+  void Update()
+  {
+    _changeStateDur -= Time.deltaTime;
+    if (_changeStateDur <= 0f)
+      SelectRandomState();
+
+    _currentState?.Update();
+  }
+
+  private void FixedUpdate()
+  {
+    _currentState?.FixedUpdate();
+  }
+
+  void SelectRandomState()
+  {
+    _currentState = _allStates[Random.Range(0, _allStates.Count)];
+    _changeStateDur = Random.Range(_currentState.ChangeStateDurMin, _currentState.ChangeStateDurMax);
+    _currentState.OnEnter();
   }
 
   public static CatControl GetClosest(Vector3 position)
@@ -54,5 +99,15 @@ public class CatControl : MonoBehaviour
   private void OnDisable()
   {
     AliveCats.Remove(this);
+  }
+
+  private void OnDrawGizmos()
+  {
+    if(_currentState == null)
+      return;
+
+#if UNITY_EDITOR
+    UnityEditor.Handles.Label(transform.position + new Vector3(1, 2, 0), _currentState.GetType().Name);
+#endif
   }
 }
