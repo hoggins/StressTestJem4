2e3662c76660d32431a37ad0e86b83092d49aefc
m.gukov <m.gukov@plarium.com>
2020-10-25 00:40:34 +0300

    add loading screen

---
+++ Unity/Assets/LoadingUiController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingUiController : MonoBehaviour
+{
+    public float PauseTime = 2;
+
+    void Start()
+    {
+        StartCoroutine(StartGame());
+    }
+
+    private IEnumerator StartGame()
+    {
+        yield return new WaitForSeconds(PauseTime);
+        SceneManager.LoadScene(UnityContract.SceneStartMenu, LoadSceneMode.Single);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
