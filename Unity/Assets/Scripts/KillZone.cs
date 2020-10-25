4228b7e5147bf3110b11831eb0a58340466586da
s.kryvych <s.kryvych@plarium.com>
2020-10-25 13:33:33 +0200

    Kill zone

---
+++ Unity/Assets/Scripts/KillZone.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+public class KillZone : MonoBehaviour
+{
+  private void Update()
+  {
+    if (transform.position.y < -100)
+    {
+      transform.position = new Vector3(0, 50, 0);
+    }
+  }
+}
